61806a713834e521595c41f7a96851482662cdd2
eugene <earzamastsev@gmail.com>
2020-02-11 11:49:17 +0500

    Adding routes to app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from  flask import Flask
 from flask import render_template
 from flask import request
-import os
-
+from data import *
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,12 +10,29 @@ app.config['DEBUG'] = True
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form['email']
-    password = request.form['password']
-    print(email, password)
-    return "OK"
+@app.route('/goals/<goal>/')
+def goal(goal):
+    return render_template('goal.html', goal=goal)
+
+@app.route('/profiles/<teacher_id>/')
+def profile(teacher_id):
+    return render_template('profile.html', teacher_id=teacher_id)
+
+@app.route('/request/')
+def request():
+    return render_template('request.html')
+
+@app.route('/request_done/', methods=['POST'])
+def request_done():
+    return render_template('request_done.html')
+
+@app.route('/booking/<teacher_id>/<day>/<time>/')
+def booking(teacher_id, day, time):
+    return render_template('booking.html', teacher_id=teacher_id, day=day, time=time)
+
+@app.route('/booking_done/', methods=['POST'])
+def booking_done():
+    return render_template('booking_done.html')
 
 if __name__ == "__main__":
     app.run()
