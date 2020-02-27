7e7b865e0db28bf5fe7b6703573e4f50d51f1ff8
eugene <earzamastsev@gmail.com>
2020-02-27 23:12:40 +0500

    Release candidate v1.0 - show appropriative goals pictures in pages

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-from  flask import Flask
+from flask import Flask
 from flask import render_template
 from flask import request as req
 import json
 import random
-from  os import path
-
-
+from os import path
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-
 # ==== start preparing data and loading MOSK-data ====
 weekdays = {
-            "mon": "Понедельник",
-            "tue": "Вторник",
-            "wed": "Среда",
-            "thu": "Четверг",
-            "fri": "Пятница",
-            "sat": "Суббота",
-            "sun": "Воскресенье"
+    "mon": "Понедельник",
+    "tue": "Вторник",
+    "wed": "Среда",
+    "thu": "Четверг",
+    "fri": "Пятница",
+    "sat": "Суббота",
+    "sun": "Воскресенье"
 }
-goals = {"travel": "Для путешествий", "study": "Для учебы", "work": "Для работы", "relocate": "Для переезда"}
+goals = {"travel": ["Для путешествий", "⛱"], "study": ["Для учебы", "🏫"], "work": ["Для работы", "🏢"],
+         "relocate": ["Для переезда", "🚜"]}
 
 with open('teachers.json', 'r') as f:
     teachers = json.load(f)
+
+
 # ==== end loading MOSK-data ====
 
 # function for working with JSON-data (store requests and booking)
@@ -41,14 +41,17 @@ def write_json(filename: str, dict_data: dict):
         with open(filename, 'w') as f:
             json.dump(tmp_, f, indent=4)
 
+
 @app.route('/')
 def index():
     teachers_for_index = random.sample(teachers, 6)
     return render_template('index.html', teachers=teachers_for_index, goals=goals)
 
+
 @app.route('/all/')
 def all():
     return render_template('index.html', teachers=teachers, goals=goals)
+
 
 @app.route('/goals/<goal>/')
 def goal(goal):
@@ -58,6 +61,7 @@ def goal(goal):
             goals_teacher.append(teacher)
     return render_template('goal.html', goals=goals, goal=goal, teachers=goals_teacher)
 
+
 @app.route('/profiles/<teacher_id>/')
 def profile(teacher_id):
     for teacher in teachers:
@@ -65,9 +69,11 @@ def profile(teacher_id):
             break
     return render_template('profile.html', goals=goals, teacher=teacher, weekdays=weekdays)
 
+
 @app.route('/request/')
 def request():
     return render_template('request.html')
+
 
 @app.route('/request_done/', methods=['POST'])
 def request_done():
@@ -81,12 +87,14 @@ def request_done():
 
     return render_template('request_done.html', data=dict_request, goals=goals)
 
+
 @app.route('/booking/<teacher_id>/<day>/<time>/')
 def booking(teacher_id, day, time):
     for teacher in teachers:
         if teacher['id'] == int(teacher_id):
             break
     return render_template('booking.html', teacher=teacher, weekdays=weekdays, day=day, time=time)
+
 
 @app.route('/booking_done/', methods=['POST'])
 def booking_done():
@@ -100,6 +108,7 @@ def booking_done():
     write_json('booking.json', dict_request)
 
     return render_template('booking_done.html', data=dict_request, day=weekdays[dict_request['clientWeekday']])
+
 
 if __name__ == "__main__":
     app.run()
